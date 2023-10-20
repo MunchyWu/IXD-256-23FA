98cea7017ca4017b02c3ab9a12d76b18bca9523b
MunchyWu <Munchywu14@gmail.com>
2023-10-20 08:29:14 -0700

    Update led_blink.py

--- class02/led_blink.py
+++ class02/led_blink.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 # blink an LED connected to pin G1 on and off
+=======
+# blink an LED connected to pin 1 on and off
+>>>>>>> 018dc8dd1fd0afd638406bbb264469aa44f52f98
 
 import os, sys, io
 import M5
@@ -6,19 +10,38 @@ from M5 import *
 from hardware import *
 import time
 
+<<<<<<< HEAD
+=======
+# global pin1 variable:
+>>>>>>> 018dc8dd1fd0afd638406bbb264469aa44f52f98
 pin1 = None
 
 def setup():
   global pin1
+<<<<<<< HEAD
 
   M5.begin()
+=======
+  # initialize M5 board:
+  M5.begin()
+  # initialize pin1 as an output pin:
+>>>>>>> 018dc8dd1fd0afd638406bbb264469aa44f52f98
   pin1 = Pin(1, mode=Pin.OUT)
 
 def loop():
   global pin1
+<<<<<<< HEAD
   M5.update()
   pin1.on()
   time.sleep(1)
+=======
+  # update M5 board:
+  M5.update()
+  # turn pin1 on (high) and sleep for 1 second:
+  pin1.on()
+  time.sleep(1)
+  # turn pin1 off (low) and sleep for 1 second:
+>>>>>>> 018dc8dd1fd0afd638406bbb264469aa44f52f98
   pin1.off()
   time.sleep(1)
 
